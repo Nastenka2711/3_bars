89faed07b34dede0fa5cf62e7ba4132fa2b3a043
Nastenka2711 <Nastasya.saurova@gmail.com>
2016-10-04 22:02:40 +0400

    Update bars.py
    Только два предупреждения pep8, касающиеся длины строки в 81, а не в 79 символов. Это же нестрашно, немного совсем выбивается?

--- bars.py
+++ bars.py
@@ -1,55 +1,65 @@
 import json
+import sys
+import os
 
 n_max_bar = 0
 n_min_bar = 10000
 geo_min = 100
-my_geo = [37.621587946152017,55.765366956608390] #совсем рядом с Юнион Джек
+my_geo = [37.621587946152017, 55.765366956608390]
 name_max_bar = []
 name_min_bar = []
 
+
 def load_data(filepath):
-	list_info_bars = json.load(filepath)
-	for list_info_bar in list_info_bars:
-		get_biggest_bar(list_info_bar)
-		get_smallest_bar(list_info_bar)
-		get_closest_bar(list_info_bar, my_geo[0], my_geo[1])
-	print("Closest bar: " + name_geo)
-	print("Max:", n_max_bar)
-	for name in name_max_bar:
-		if name['number'] == n_max_bar:
-			print(name['name'])
-	print("Min:", n_min_bar)
-	for name in name_min_bar:
-		if name['number'] == n_min_bar:
-			print(name['name'])
+    list_info_bars = json.load(filepath)
+    for list_info_bar in list_info_bars:
+        get_biggest_bar(list_info_bar)
+        get_smallest_bar(list_info_bar)
+        get_closest_bar(list_info_bar, my_geo[0], my_geo[1])
+    print("Closest bar: " + name_geo)
+    print("Max:", n_max_bar)
+    for name in name_max_bar:
+        if name['number'] == n_max_bar:
+            print(name['name'])
+    print("Min:", n_min_bar)
+    for name in name_min_bar:
+        if name['number'] == n_min_bar:
+            print(name['name'])
 
 
 def get_biggest_bar(data):
-	global n_max_bar
-	global name_max_bar
-	if data['Cells']['SeatsCount'] >= n_max_bar:
-		n_max_bar = data['Cells']['SeatsCount']
-		name_max_bar.append({'name': data['Cells']['Name'], 'number': n_max_bar})
+    global n_max_bar
+    global name_max_bar
+    if data['Cells']['SeatsCount'] >= n_max_bar:
+        n_max_bar = data['Cells']['SeatsCount']
+        name_max_bar.append({'name': data['Cells']['Name'], 'number': n_max_bar})
 
 
 def get_smallest_bar(data):
-	global n_min_bar
-	global name_min_bar
-	if data['Cells']['SeatsCount'] <= n_min_bar:
-		n_min_bar = data['Cells']['SeatsCount']
-		name_min_bar.append({'name': data['Cells']['Name'], 'number': n_min_bar})
+    global n_min_bar
+    global name_min_bar
+    if data['Cells']['SeatsCount'] <= n_min_bar:
+        n_min_bar = data['Cells']['SeatsCount']
+        name_min_bar.append({'name': data['Cells']['Name'], 'number': n_min_bar})
 
 
 def get_closest_bar(data, longitude, latitude):
-	global geo_min 
-	global name_geo
-	x = data['Cells']['geoData']['coordinates'][0] - longitude
-	y = data['Cells']['geoData']['coordinates'][1] - latitude
-	xy = x ** 2 + y ** 2
-	if xy < geo_min:
-		geo_min = xy
-		name_geo = data['Cells']['Name']	
+    global geo_min
+    global name_geo
+    x = data['Cells']['geoData']['coordinates'][0] - longitude
+    y = data['Cells']['geoData']['coordinates'][1] - latitude
+    xy = x ** 2 + y ** 2
+    if xy < geo_min:
+        geo_min = xy
+        name_geo = data['Cells']['Name']
 
 
 if __name__ == '__main__':
-	load_data(open('bars.json','r'))
+    if len(sys.argv) == 1:
+        name_file = input("Введите название файла  ")
+    else:
+        name_file = sys.argv[1]
+    if os.path.exists(name_file) and os.path.isfile(name_file):
+        load_data(open(name_file, 'r'))
+    else:
+        print("Ошибка открытия файла")
