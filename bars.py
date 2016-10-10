fbc01686255367d432b67aa649b4853e88fb6324
Nastenka2711 <Nastasya.saurova@gmail.com>
2016-10-10 12:36:45 +0400

    Update bars.py
    Подправила ещё то, что было в замечаниях к 11 задаче (возврат функций). Изменила функции поиска нужных баров так, чтобы печать была в main. Думаю не нужно выносить печать в отдельную функцию?

--- bars.py
+++ bars.py
@@ -25,52 +25,51 @@ def input_geo_data():
 
 
 def file_exists(filepath):
-    if os.path.exists(filepath) and os.path.isfile(filepath):
-        return True
+    return os.path.exists(filepath) and os.path.isfile(filepath)
 
 
 def check_json(file_name):
-    if file_name.endswith('.json'):
-        return True
+    return file_name.endswith('.json')
 
 
 def search_bar_max(bars_list):
-    bar_max = max(bars_list, key=lambda l: l['Cells']['SeatsCount'])
-    print("%s %d: %s" % ("Бар с максимальным числом посадочных мест",
-                         bar_max['Cells']['SeatsCount'],
-                         bar_max['Cells']['Name']))
+    return max(bars_list, key=lambda l: l['Cells']['SeatsCount'])
 
 
 def search_bar_min(bars_list):
-    bar_min = min(bars_list, key=lambda l: l['Cells']['SeatsCount'])
-    print("%s %d: %s" % ("Бар с минимальным числом посадочных мест",
-                         bar_min['Cells']['SeatsCount'],
-                         bar_min['Cells']['Name']))
+    return min(bars_list, key=lambda l: l['Cells']['SeatsCount'])
 
 
 def search_bar_near(bars_list):
     geo_data = input_geo_data()
     x = sqrt(geo_data[0]**2 + geo_data[1]**2)
-    bar_near = min(bars_list, key=lambda l:
-                   abs(sqrt(l['Cells']['geoData']['coordinates'][0]**2 +
-                       l['Cells']['geoData']['coordinates'][1]**2)-x))
-    print("%s %s: %s" % ("Ближайший к Вам бар с координатами",
-                         bar_near['Cells']['geoData']['coordinates'],
-                         bar_near['Cells']['Name']))
+    return min(bars_list, key=lambda l:
+               abs(sqrt(l['Cells']['geoData']['coordinates'][0]**2 +
+                        l['Cells']['geoData']['coordinates'][1]**2)-x))
 
 
 def load_data(filepath):
-    bars_list = json.load(filepath)
-    search_bar_near(bars_list)
-    search_bar_max(bars_list)
-    search_bar_min(bars_list)
+    return json.load(filepath)
 
 
 if __name__ == '__main__':
     file_name = input_file_name()
-    if check_json(file_name) is None:
+    if not check_json(file_name):
         print("Формат файла должен быть .json")
-    elif file_exists(file_name) is None:
+    elif not file_exists(file_name):
         print("Ошибка открытия файла")
     else:
-        load_data(open(file_name, 'r'))
+        bars_list = load_data(open(file_name, 'r'))
+        bar_near = search_bar_near(bars_list)
+        print("%s %s: %s" % ("Ближайший к Вам бар с координатами",
+                             bar_near['Cells']['geoData']['coordinates'],
+                             bar_near['Cells']['Name']))
+        bar_max = search_bar_max(bars_list)
+        print("%s %d: %s" % ("Бар с максимальным числом посадочных мест",
+                             bar_max['Cells']['SeatsCount'],
+                             bar_max['Cells']['Name']))
+
+        bar_min = search_bar_min(bars_list)
+        print("%s %d: %s" % ("Бар с минимальным числом посадочных мест",
+                             bar_min['Cells']['SeatsCount'],
+                             bar_min['Cells']['Name']))
